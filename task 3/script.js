90e092558fd0ffe46098217e079b81a5ccfaa6a1
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-04-29 21:37:49 +0300

    add first changes

---
+++ task 3/script.js
@@ -0,0 +1,57 @@
+function SuperArray(n,m,obj) {
+   
+    var massive = new Array(n)
+    
+    for(var i = 0; i < massive.length; i++){
+        massive[i] = new Array(m);
+    }
+
+    for(i = 0; i < massive.length; i++){
+        for(var j = 0; j < massive[i].length; j++){
+            massive[i][j] = Math.floor(Math.random()*(obj.max-obj.min+1)+obj.min);
+        }
+    } 
+    this.arr = massive
+
+}
+
+SuperArray.prototype.render = function(separator){
+        separator = separator||'*'
+    for(var i = 0; i < this.arr.length; i++){
+        for(var j = 0; j < this.arr[i].length; j++){
+            document.write(this.arr[i][j] + ' ');
+        }
+        document.write('<br />');
+    }
+    for(i = 0; i<this.arr[0].length;i++){
+        document.write(separator + " ");
+    }
+    document.write('<br />');
+}
+
+var sArr = new SuperArray(20,50, {min: 0, max: 9})
+
+SuperArray.prototype.clear = function(direction, k) {
+    
+    for(var i = 0; i < this.arr.length; i++){
+        for(var j = 0; j < this.arr[i].length; j++){
+            if(direction == 'row') {
+                this.arr[k][j] = '0'
+            }
+
+            if(direction == "column") {
+                this.arr[i][k] = '0'
+            }
+           
+        }
+    }
+        this.render()
+}
+
+
+SuperArray.prototype.setMarker = function(obj){
+            this.buffer = this.arr[obj.x][obj.y]
+            this.arr[obj.x][obj.y] = "&"
+    
+    this.render()
+}

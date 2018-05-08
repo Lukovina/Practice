9ba4e065dead7c88061da78b42dff92fbdd4d200
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-05-08 23:12:26 +0300

    finish version 1 and start version 2

--- Homework13/lesson practice/script.js
+++ Homework13/lesson practice/script.js
@@ -6,19 +6,31 @@ window.onload = function() {
     
     var i = 0;
     var step = 1;
-    
-   
+
+       
     setInterval(function(){
-            if(i == blocks.length) {
-                i = 0;
-            }
+        
+        if(i == blocks.length - 1) {
+            step = -1
+        }else if(i == 0) {
+            step = 1
+        }
+        
+        blocks[i].classList.toggle('bottom');
+       
+        i+=step;
+       
+    }, 300)
+    // VERSION 1 
+    // setInterval(function(){
+    //         if(i == blocks.length) {
+    //             i = 0;
+    //         }
             
-            blocks[i].classList.toggle('bottom');
+    //         blocks[i].classList.toggle('bottom');
             
-            
-            
-            console.log('hello')
-            i+=step;
+
+    //         i+=step;
            
-        }, 300)
+    //     }, 300)
     }

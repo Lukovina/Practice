de435a75dcf8597cf065e86dd9a36d3827f04b14
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-06-03 19:40:22 +0300

    some changes in 19HW

--- Homework19/script.js
+++ Homework19/script.js
@@ -4,75 +4,55 @@ window.onload = function () {
     var add_bottoms = document.querySelectorAll(".Add_btn");
     var remove_btn = document.querySelector(".remove");
     var remove_item_bottoms = document.querySelectorAll(".Remove_item");
-    
+    var goods = JSON.parse(xhr.responseText);
 
 
-
-var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'goods.json',true);
-    xhr.send();
-    
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) return;
+    add_bottoms.forEach(function(item){
+        item.addEventListener('click', addToCard)
+        });
         
-            var goods = JSON.parse(xhr.responseText);
-            console.log(goods)
-
-            add_bottoms.forEach(function(item){
-                item.addEventListener('click', addToCard)
-            });
+    remove_item_bottoms.forEach(function(item){
+        item.addEventListener('click', removeFromCard)
+        });
         
-            remove_item_bottoms.forEach(function(item){
-                item.addEventListener('click', removeFromCard)
-            });
+    remove_btn.addEventListener('click', clearStorage);
         
-            remove_btn.addEventListener('click', clearStorage);
-        
-            if(localStorage.getItem("goods")){
-                var storageGoods = JSON.parse(localStorage.goods);
-                renderStorage();
-            }else {
-                  var storageGoods = {}
+    if(localStorage.getItem("goods")){
+        var storageGoods = JSON.parse(localStorage.goods);
+        renderStorage();
+        }else {
+            var storageGoods = {}
             }
           
-            function addToCard(event) {
-                if(!storageGoods[this.parentElement.id]) {
-                    storageGoods[this.parentElement.id] = goods[this.parentElement.id];
-                }
-                console.log(JSON.stringify(goods))
-                storageGoods[this.parentElement.id].quantity +=1;
-                storageGoods = JSON.stringify(storageGoods);
-                localStorage.setItem("goods", storageGoods);
-                storageGoods = JSON.parse(localStorage.goods);
-                renderStorage()
+    function addToCard(event) {
+        if(!storageGoods[this.parentElement.id]) {
+        storageGoods[this.parentElement.id] = goods[this.parentElement.id];
+             }
+        storageGoods[this.parentElement.id].quantity +=1;
+        localStorage.setItem("goods", JSON.stringify(storageGoods));
+        renderStorage()
             }
         
-            function removeFromCard() {
-                storageGoods = JSON.parse(localStorage.goods);
-                delete storageGoods[this.parentElement.id];
-                storageGoods = JSON.stringify(storageGoods);
-                localStorage.setItem("goods", storageGoods);
-                storageGoods = JSON.parse(localStorage.goods);
-                renderStorage()
+    function removeFromCard() {
+        storageGoods[this.parentElement.id].quantity = 0;
+        delete storageGoods[this.parentElement.id];
+        localStorage.setItem("goods", JSON.stringify(storageGoods));
+        renderStorage()
             }
         
-            function clearStorage(){
-                storageGoods = "{}";
-                localStorage.setItem("goods", storageGoods);
-                storageGoods = JSON.parse(storageGoods);
-                renderStorage();
+    function clearStorage(){
+        storageGoods = {};
+        localStorage.setItem("goods", JSON.stringify(storageGoods));
+        renderStorage();
             }
         
-            function renderStorage() {
-                var ls = JSON.parse(localStorage.goods);
-        
-                storage.innerHTML = "<tr>";
-                for (var key in ls) {
-                    storage.innerHTML += "<td>" + ls[key].name + "</td>" +  "<td>" + "&nbsp x &nbsp" + ls[key].quantity + "</td>"
-                }
-                storage.innerHTML += "</tr>";
+    function renderStorage() {
+        var ls = JSON.parse(localStorage.goods);
+        storage.innerHTML = "<tr>";
+        for (var key in ls) {
+        storage.innerHTML += "<td>" + ls[key].name + "</td>" +  "<td>" + "&nbsp x &nbsp" + ls[key].quantity + "</td>"
             }
+        storage.innerHTML += "</tr>";
         }
         
         
@@ -80,4 +60,6 @@ var xhr = new XMLHttpRequest();
     
    
 
-   
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'goods.json',true);
+    xhr.send();

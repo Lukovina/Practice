548a0cb95340575db3b927f27eb65739e3f7a333
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-06-12 23:44:05 +0300

    add 23 classwork with set/getters

---
+++ Homework23/lesson practice/script.js
@@ -0,0 +1,52 @@
+// let data = {
+
+//     set model (value) {
+//         if(!this.buffer) {
+//             this.buffer = value   
+//             return     
+//         }
+//            this.str = this.buffer.split(value),
+//            this.separator = value 
+//     },
+
+//     get model () {
+//         let result = {
+//             symbol: this.separator,
+//             count: this.str.length - 1
+//         }
+//         return result
+//     }
+// }
+
+// data.model = "Hello, how, are you?"
+//----------------------------------
+
+const object = {
+    x: 10,
+    y: 20,
+set model (obj) {
+
+    var actions = {
+        "*" : (a,b) => a * b,
+        "+" : (a,b) => a + b
+    }
+   
+    for(var key in obj) {
+        this[key] =  actions[obj[key].operation](this.key, obj[key].value)
+        } 
+    }
+}
+
+const obj2 = {
+    x: {value: 33,
+    operation: "*"
+    },
+    y: {value: 75,
+    operation: '+'},
+
+}
+
+
+
+object.model = obj2
+

ae75a1891472a64a72375997e1cfef8cf5bd821b
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-06-14 18:06:30 +0300

    Homework 32, task 1

--- Homework23/lesson practice/script.js
+++ Homework23/lesson practice/script.js
@@ -1,3 +1,5 @@
+// ----------Lesson practice
+// ----------task1
 // let data = {
 
 //     set model (value) {
@@ -19,34 +21,57 @@
 // }
 
 // data.model = "Hello, how, are you?"
-//----------------------------------
+//----------task2
 
-const object = {
-    x: 10,
-    y: 20,
-set model (obj) {
+// const object = {
+//     x: 10,
+//     y: 20,
+// set model (obj) {
 
-    var actions = {
-        "*" : (a,b) => a * b,
-        "+" : (a = 0,b = 0) => a + b
-    }
+//     var actions = {
+//         "*" : (a,b) => a * b,
+//         "+" : (a = 0,b = 0) => a + b
+//     }
    
-    for(var key in obj) {
-        this[key] =  actions[obj[key].operation](this[key], obj[key].value)
-        } 
-    }
+//     for(var key in obj) {
+//         this[key] =  actions[obj[key].operation](this[key], obj[key].value)
+//         } 
+//     }
+// }
+
+// const obj2 = {
+//     x: {value: 33,
+//     operation: "*"
+//     },
+//     z: {value: 75,
+//     operation: '+'
+//     }
+// }
+
+// object.model = obj2
+
+//---------Homework
+
+//---------task1
+
+// const obj = {
+//     list_1: [10, 20, 30, 40, 50, 60],
+//     list_2: [15, 30, 45, 60, 75, 90, 105],
+//     get collision () {
+        
+//         return this.list_1.filter(function(item) {
+//             for(var key of obj.list_2) {
+//                 if(item == key) {
+//                     return item
+//                 }
+//             } 
+//         })
+//     }  
+// }
+
+//--------task2
+
+Array.prototype.get = function () {
+
 }
-
-const obj2 = {
-    x: {value: 33,
-    operation: "*"
-    },
-    z: {value: 75,
-    operation: '+'},
-
-}
-
-
-
-object.model = obj2
 

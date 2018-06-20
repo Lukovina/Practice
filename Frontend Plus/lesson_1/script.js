6b7930ab8f366d7ebe878ed213844ce15f15c205
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-06-20 17:10:48 +0300

    add materialize styles to 1 HW 2 task

--- Frontend Plus/lesson_1/script.js
+++ Frontend Plus/lesson_1/script.js
@@ -16,8 +16,9 @@
 
 
 //task2
+
 let objList = [
-               { title: 'Owl', size: 4, img: 'http://www.catersnews.com/wp-content/uploads/2017/01/0_CATERS_OWL_WITH_HEART_SHAPED_FACE_01-800x498.jpg' },
+               { title: 'Owl', size: 4, img: 'https://www.version2.dk/sites/v2/files/topillustration/2017/07/5927758528a2060423e7b.jpg' },
                { title: 'Seagull', size: 35, img: 'https://www.samcodes.co.uk/project/geometrize-haxe-web/assets/images/xseagull.jpg.pagespeed.ic.iK66EGA15-.jpg' }, 
                { title: 'Bear', size: 22, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-WXtjHQNadRCsA0EWNNe_QyzX-jGpi83_zKg3seOAF6-2bupYbQ' }          
         ]
@@ -25,21 +26,39 @@ let objList = [
 
 class ViewController {
     constructor(model) {
-        this.model
+        this.model = model
+    }
+    get render () {
+        return  `<div class = "container">
+                    <div class = "row">
+                        ${this.model.map(item=>
+                            `<div class = "col s12 m6 l3">
+                                <div class = "card ">
+                                    <div class = "card-image">
+                                    <img src = "${item.img}" >
+                                    </div>
+                                    <div class = "card-content">
+                                        <p class = "card-title"> ${item.title}</p>
+                                        <span>size: ${item.size}</span>
+                                    </div>
+                                </div>
+                             </div>`
+                        ).join("")}
+                    </div>    
+                </div>`
     }
 
-    render (list) {
-        return list.map(item => 
-                `<div class="card">
-                    <img src = "${item.img}"> 
-                    <h2>${item.title}</h2>
-                    <p>${item.size}</p> 
-                </div>`).join("")
-         }
+    get sortBySize () {
+        this.model.sort((a,b) => a.size-b.size)
+        return this
+    }     
 }
+var viewHolder = new ViewController(objList);
+
 
 
 window.onload = function() {
-    document.body.innerHTML = card
+    document.body.innerHTML = viewHolder.sortBySize.render
 }
+
 

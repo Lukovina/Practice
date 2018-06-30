1e110a4aaae5c0ff509ca01a9852693a395e8e6c
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-06-30 19:27:09 +0300

    add 2 HW , 1 task

--- Frontend/Homework27/js/app.js
+++ Frontend/Homework27/js/app.js
@@ -3,8 +3,8 @@ var render = require('./render.js');
 
 dataService.load("GET", "https://api.github.com/orgs/hillel-front-end/repos")
     .then(val=>val.map(item=>item.forks_url)) 
-    .then(list=>list.map(item=>dataService.load("GET", item)))
-    .then(res=>Promise.all(res))
+    .then(list=>{console.log(list); return list.map(item=>dataService.load("GET", item))})
+    .then(res=>{ console.log(res); return Promise.all(res)})
     .then(array=>array.forEach(arr=>{
         console.log(arr)
     }))

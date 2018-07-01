8bf2046a702ada64a7af9382889d2b661110bd7d
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-07-02 00:20:24 +0300

    some render fixes, 27 HW

--- Frontend/Homework27/js/app.js
+++ Frontend/Homework27/js/app.js
@@ -8,7 +8,9 @@ let tableItems = {
 
 
 dataService.load("GET", "https://api.github.com/orgs/hillel-front-end/repos")
-    .then(val=> {tableItems.repos = val;  return val.map(item=>item.forks_url)} ) 
+    .then(val=> {
+        tableItems.repos = val;  
+        return val.map(item=>item.forks_url)} ) 
     .then(list=>list.map(item=>dataService.load("GET", item)))
     .then(res=>Promise.all(res))
     .then(array=>array.forEach(arr=> {tableItems.forks.push(arr)}))

c46d0a375b772154eb7b7d1e6ce7744584455d9b
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2018-07-02 00:01:13 +0300

    some render fixes, 27 HW

--- Frontend/Homework27/js/app.js
+++ Frontend/Homework27/js/app.js
@@ -2,13 +2,13 @@ var dataService = require('./dataService.js');
 var render = require('./render.js');
 
 let tableItems = {
-    names: [],
+    repos: [],
     forks: []
 }
 
 
 dataService.load("GET", "https://api.github.com/orgs/hillel-front-end/repos")
-    .then(val=> {tableItems.names = val.map(repo=>repo.name);  return val.map(item=>item.forks_url)} ) 
+    .then(val=> {tableItems.repos = val;  return val.map(item=>item.forks_url)} ) 
     .then(list=>list.map(item=>dataService.load("GET", item)))
     .then(res=>Promise.all(res))
     .then(array=>array.forEach(arr=> {tableItems.forks.push(arr)}))
--- Frontend/Homework27/js/render.js
+++ Frontend/Homework27/js/render.js
@@ -1,6 +1,21 @@
 function render(obj) {
-    console.log(obj)
+    let names = obj.repos.map(repo=>repo.name),
+        forkOwners = obj.forks.map(fork => fork.map(arr=>arr.full_name)),
+        tableHolder = document.querySelector(".repo-table")
+
+    for(let key in names) {
+        tableHolder.innerHTML +=
+        `<li>
+        ${names[key]}
+            <ul>
+                ${forkOwners[key].map(name=>`<li>${name}</li>`).join("")}
+            <ul>
+        </li>`
+
+    }
+   
 }
+
 
 module.exports = {
     render
